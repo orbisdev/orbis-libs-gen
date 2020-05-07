bcddd42965f62ba4f9c2e96b12f87b72e0968952
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-05-07 15:34:09 +0200

    Make optionals shared_object_name and shared_objects_names

--- Sources/orbis-parser/OrbisSPRX.swift
+++ Sources/orbis-parser/OrbisSPRX.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 struct OrbisSPRX: Codable {
-    let shared_object_name: String
-    let shared_object_names: [String]
+    let shared_object_name: String?
+    let shared_object_names: [String]?
     let modules: [OrbisModule]
     
     var assembly: String { modules.reduce("") { $0 + $1.assembly } }
