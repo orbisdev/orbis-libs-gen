859c8125de156104e2c4146df2beacd6855e6d08
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-05-07 12:03:11 +0200

    Testing folder iteration

--- Sources/orbis-parser/OrbisParser.swift
+++ Sources/orbis-parser/OrbisParser.swift
@@ -2,19 +2,23 @@ import Foundation
 
 private enum Constants {
     static let outputPath = URL(fileURLWithPath: "output")
+    static let inputPath = URL(fileURLWithPath: "ps4libdoc")
 }
 
 class OrbisParser {
-    func parse() {
+    init() {
         prepare()
-        
+    }
+    
+    func parse() {
+        printFolders()
         let fileName = "libScePad"
         let fileURL = URL(fileURLWithPath: "\(fileName).sprx.json")
-        
+
         guard let data = try? Data(contentsOf: fileURL) else { return }
         do {
             let sprx = try JSONDecoder().decode(OrbisSPRX.self, from: data)
-            
+
             createAssemblyFile("\(fileName).s", content: sprx.assembly)
         } catch {
             print(error)
@@ -36,6 +40,21 @@ class OrbisParser {
         // Remove and create output dir
         try? FileManager.default.removeItem(at: Constants.outputPath)
         try? FileManager.default.createDirectory(at: Constants.outputPath, withIntermediateDirectories: true, attributes: nil)
+    }
+    
+    private func printFolders() {
+        var files = [URL]()
+        if let enumerator = FileManager.default.enumerator(at: Constants.inputPath, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+            for case let fileURL as URL in enumerator {
+                do {
+                    let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
+                    if fileAttributes.isRegularFile! {
+                        files.append(fileURL)
+                    }
+                } catch { print(error, fileURL) }
+            }
+            print(files)
+        }
     }
 
 }
