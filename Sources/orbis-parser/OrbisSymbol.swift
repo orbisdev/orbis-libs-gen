2d711ba4e30454b3334e1b12c3eff27a0cc1702b
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-05-07 19:41:46 +0200

    Add type property

--- Sources/orbis-parser/OrbisSymbol.swift
+++ Sources/orbis-parser/OrbisSymbol.swift
@@ -1,16 +1,25 @@
 import Foundation
 
 struct OrbisSymbol: Codable {
+    enum SymbolType: String, Codable {
+        case Object
+        case Function
+        case TLS
+        case Unknown11
+    }
+
     let id: Double
     let name: String?
     let hex_id: String
     let encoded_id: String
+    let type: SymbolType?
+    private var assemblyType: String { self.type == .Object ? "@object" : "@function" }
     
     var assembly: String {
         guard let name = name else { return ""}
         return """
             .global \(name)
-            .type \(name), @function
+            .type \(name), \(assemblyType)
             __\(name):
                 .quad 0x\(hex_id)
             
