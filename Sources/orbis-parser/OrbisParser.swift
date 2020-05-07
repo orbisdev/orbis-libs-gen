984ca455cf9e973fdf7684a9252b98ae4c500fbb
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-05-07 15:21:20 +0200

    Now the project is parsing all the files that are in the ps4libdoc

--- Sources/orbis-parser/OrbisParser.swift
+++ Sources/orbis-parser/OrbisParser.swift
@@ -3,29 +3,46 @@ import Foundation
 private enum Constants {
     static let outputPath = URL(fileURLWithPath: "output")
     static let inputPath = URL(fileURLWithPath: "ps4libdoc")
+    static let sprxFiles = ".sprx.json"
+    static let sFiles = ".s"
 }
 
 class OrbisParser {
-    init() {
-        prepare()
+    init() { prepare() }
+    func parse() { filesToParse.forEach { parseFile($0) } }
+}
+
+// MARK: - Private Methods
+private extension OrbisParser {
+    private var filesToParse: [URL] {
+        guard let enumerator = FileManager.default.enumerator(at: Constants.inputPath,
+                                                              includingPropertiesForKeys: [.isRegularFileKey],
+                                                              options: [.skipsHiddenFiles, .skipsPackageDescendants])
+        else { return [] }
+        
+        return enumerator.reduce(into: [URL]()) {
+            guard let fileURL = $1 as? URL, fileURL.absoluteString.hasSuffix(Constants.sprxFiles),
+            let attrs = try? fileURL.resourceValues(forKeys:[.isRegularFileKey]),
+            attrs.isRegularFile == true  else { return }
+            $0.append(fileURL)
+        }
     }
     
-    func parse() {
-        printFolders()
-        let fileName = "libScePad"
-        let fileURL = URL(fileURLWithPath: "\(fileName).sprx.json")
+    func parseFile(_ fileURL: URL) {
+        let filename = fileURL.lastPathComponent.replacingOccurrences(of: Constants.sprxFiles, with: Constants.sFiles)
 
         guard let data = try? Data(contentsOf: fileURL) else { return }
         do {
             let sprx = try JSONDecoder().decode(OrbisSPRX.self, from: data)
 
-            createAssemblyFile("\(fileName).s", content: sprx.assembly)
+            createAssemblyFile(filename, content: sprx.assembly)
         } catch {
+            print(fileURL.lastPathComponent)
             print(error)
         }
     }
     
-    private func createAssemblyFile(_ fileName: String, content: String) {
+    func createAssemblyFile(_ fileName: String, content: String) {
         let fileManager = FileManager.default
         let newFile = Constants.outputPath.appendingPathComponent(fileName).path
         
@@ -36,25 +53,9 @@ class OrbisParser {
         }
     }
     
-    private func prepare() {
+    func prepare() {
         // Remove and create output dir
         try? FileManager.default.removeItem(at: Constants.outputPath)
         try? FileManager.default.createDirectory(at: Constants.outputPath, withIntermediateDirectories: true, attributes: nil)
     }
-    
-    private func printFolders() {
-        var files = [URL]()
-        if let enumerator = FileManager.default.enumerator(at: Constants.inputPath, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
-            for case let fileURL as URL in enumerator {
-                do {
-                    let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
-                    if fileAttributes.isRegularFile! {
-                        files.append(fileURL)
-                    }
-                } catch { print(error, fileURL) }
-            }
-            print(files)
-        }
-    }
-
 }
